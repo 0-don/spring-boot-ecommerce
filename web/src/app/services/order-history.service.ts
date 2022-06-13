49b2b3613b5e483710ff755bc4a1c65a91cb5500
Don-Cryptus <don.cryptus@gmail.com>
2022-06-13 23:11:56 +0200

    fix orders

--- web/src/app/services/order-history.service.ts
+++ web/src/app/services/order-history.service.ts
@@ -13,7 +13,7 @@ export class OrderHistoryService {
 
   getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory> {
     // need to build URL based on the customer email
-    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOOrderByDateCreatedDesc?email=${theEmail}`;
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=${theEmail}`;
 
     return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
   }
