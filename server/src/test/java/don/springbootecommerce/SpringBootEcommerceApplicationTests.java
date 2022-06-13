5b616b4d57edfbbe3c3b0c17d36f9784b167a4ef
Don-Cryptus <don.cryptus@gmail.com>
2022-06-13 23:15:19 +0200

    add dependencies and env

--- server/src/test/java/don/springbootecommerce/SpringBootEcommerceApplicationTests.java
+++ server/src/test/java/don/springbootecommerce/SpringBootEcommerceApplicationTests.java
@@ -1,9 +1,11 @@
 package don.springbootecommerce;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication()
 class SpringBootEcommerceApplicationTests {
 
 	@Test
