a9bc8062e3d6cd8ceba7335b1790839319b9a021
Don-Cryptus <don.cryptus@gmail.com>
2022-06-17 22:25:39 +0200

    change url

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: `http://localhost:8080`,
+  serverUrl: `https://ecommerce.myngz.com`,
   stripePublishableKey:
     'pk_test_51HzuVwBO89hznhCztQ4D1tFaxug9go1yJSAKNwPuEaZ5F03sJbKrCBBT13bXpdjyZ5dkCdvJGeaqrLUHOGB6DHqb000zbuRLZD',
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: `https://ecommerce.myngz.com`,
+  serverUrl: `http://localhost:8080`,
   stripePublishableKey:
     'pk_test_51HzuVwBO89hznhCztQ4D1tFaxug9go1yJSAKNwPuEaZ5F03sJbKrCBBT13bXpdjyZ5dkCdvJGeaqrLUHOGB6DHqb000zbuRLZD',
 };
