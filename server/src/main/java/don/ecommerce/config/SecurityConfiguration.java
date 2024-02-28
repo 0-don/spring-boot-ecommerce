cd23ae961242cc2143484485a092c31a2cd1fcdc
don.cryptus <don.cryptus@gmail.com>
2024-02-29 00:48:12 +0100

    easy

--- server/src/main/java/don/ecommerce/config/SecurityConfiguration.java
+++ server/src/main/java/don/ecommerce/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package don.ecommerce.config;
 
-import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
