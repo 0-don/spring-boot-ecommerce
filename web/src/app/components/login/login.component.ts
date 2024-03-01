9d890aff4f6ec602c16d0458e8e87dceae210e54
don.cryptus <don.cryptus@gmail.com>
2024-03-01 23:37:23 +0100

    easy

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -6,21 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  oktaSignin: any;
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.oktaSignin.remove();
-
-    this.oktaSignin.renderEl(
-      {
-        el: '#okta-sign-in-widget',
-      }, // this name should be same as div tag id in login.component.html
-      (response: { status: string }) => {},
-      (error: any) => {
-        throw error;
-      }
-    );
-  }
+  ngOnInit(): void {}
 }
