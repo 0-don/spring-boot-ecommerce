6b8f7173c16fbbddaf14fa2af18a50a14194c216
don.cryptus <don.cryptus@gmail.com>
2024-03-01 23:23:37 +0100

    easy

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
+  darkMode: 'class',
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
