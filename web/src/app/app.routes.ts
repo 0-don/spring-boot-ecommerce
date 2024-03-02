4c03cde535f163372d488aba4f8d70fee9ea3566
don.cryptus <don.cryptus@gmail.com>
2024-03-02 01:04:37 +0100

    x

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login/login.component';
-import { HomeComponent } from './features/main/home/home/home.component';
+import { HomeComponent } from './features/main/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
