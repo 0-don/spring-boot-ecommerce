c57894ad76b89b55b78b9d1e7dfde4898c34d14e
don.cryptus <don.cryptus@gmail.com>
2024-03-03 16:24:30 +0100

    easy

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,6 +3,7 @@ const { join } = require("path");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
+  darkMode: "class",
   content: [
     join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}"),
     join(__dirname, "components/**/*.{ts,html}"),
