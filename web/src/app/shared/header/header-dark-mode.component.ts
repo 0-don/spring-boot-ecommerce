070e5bbaaa33bd21652bd4ed8a5b912d60f2fd3b
don.cryptus <don.cryptus@gmail.com>
2024-03-03 19:00:34 +0100

    y

--- web/src/app/shared/header/header-dark-mode.component.ts
+++ web/src/app/shared/header/header-dark-mode.component.ts
@@ -6,40 +6,65 @@ import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
 import { HlmMenuComponent, HlmMenuImports } from '@spartan-ng/ui-menu-helm';
-import { DarkMode, ThemeService } from '../theme.service';
+import { ThemeService, DarkMode } from '../service/theme.service';
 
 @Component({
-	selector: 'spartan-dark-mode',
-	standalone: true,
-	imports: [BrnMenuTriggerDirective, HlmMenuImports, HlmButtonDirective, HlmIconComponent, AsyncPipe, HlmMenuComponent],
-	providers: [provideIcons({ lucideMoon })],
-	template: `
-		<button size="sm" variant="ghost" align="end" [brnMenuTriggerFor]="theme" hlmBtn>
-			<hlm-icon name="lucideMoon" size="sm" />
-			<span class="sr-only">Open menu to change theme</span>
-		</button>
-		<ng-template #theme>
-			<hlm-menu class="w-40">
-				<button hlmMenuItemCheckbox [checked]="(theme$ | async) === 'light'" (click)="setTheme('light')">
-					<hlm-menu-item-check />
-					Light
-				</button>
-				<button hlmMenuItemCheckbox [checked]="(theme$ | async) === 'dark'" (click)="setTheme('dark')">
-					<hlm-menu-item-check />
-					Dark
-				</button>
-				<button hlmMenuItemCheckbox [checked]="(theme$ | async) === 'system'" (click)="setTheme('system')">
-					<hlm-menu-item-check />
-					System
-				</button>
-			</hlm-menu>
-		</ng-template>
-	`,
+  selector: 'spartan-dark-mode',
+  standalone: true,
+  imports: [
+    BrnMenuTriggerDirective,
+    HlmMenuImports,
+    HlmButtonDirective,
+    HlmIconComponent,
+    AsyncPipe,
+    HlmMenuComponent,
+  ],
+  providers: [provideIcons({ lucideMoon })],
+  template: `
+    <button
+      size="sm"
+      variant="ghost"
+      align="end"
+      [brnMenuTriggerFor]="theme"
+      hlmBtn
+    >
+      <hlm-icon name="lucideMoon" size="sm" />
+      <span class="sr-only">Open menu to change theme</span>
+    </button>
+    <ng-template #theme>
+      <hlm-menu class="w-40">
+        <button
+          hlmMenuItemCheckbox
+          [checked]="(theme$ | async) === 'light'"
+          (click)="setTheme('light')"
+        >
+          <hlm-menu-item-check />
+          Light
+        </button>
+        <button
+          hlmMenuItemCheckbox
+          [checked]="(theme$ | async) === 'dark'"
+          (click)="setTheme('dark')"
+        >
+          <hlm-menu-item-check />
+          Dark
+        </button>
+        <button
+          hlmMenuItemCheckbox
+          [checked]="(theme$ | async) === 'system'"
+          (click)="setTheme('system')"
+        >
+          <hlm-menu-item-check />
+          System
+        </button>
+      </hlm-menu>
+    </ng-template>
+  `,
 })
 export class HeaderDarkModeComponent {
-	private _themeService = inject(ThemeService);
-	theme$ = this._themeService.darkMode$;
-	public setTheme(theme: DarkMode) {
-		this._themeService.setDarkMode(theme);
-	}
+  private _themeService = inject(ThemeService);
+  theme$ = this._themeService.darkMode$;
+  public setTheme(theme: DarkMode) {
+    this._themeService.setDarkMode(theme);
+  }
 }
