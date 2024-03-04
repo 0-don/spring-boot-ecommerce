865689d225036db87025d2fe758d8dbc8fec3933
don.cryptus <don.cryptus@gmail.com>
2024-03-04 20:21:56 +0100

    d

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,7 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import {
+  HttpBackend,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export const HttpLoaderFactory = (httpClient: HttpClient) =>
+  new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpBackend],
+  },
+});
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      HttpClientModule,
+      TranslateModule.forRoot(provideTranslation()),
+    ]),
+  ],
 };
