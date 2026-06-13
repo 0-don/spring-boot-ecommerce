cdde5ebf053baf4b3b58fa8081450d5211748436
0-don <don.cryptus@gmail.com>
2026-06-14 00:33:01 +0200

    chore: migrate spring-boot-ecommerce to coding-global.com

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 import { KeycloakConfig } from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
-  url: 'https://auth.coding.global',
+  url: 'https://auth.coding-global.com',
   realm: 'ecommerce',
   clientId: 'angular-client',
 };
 
 export const environment = {
   production: false,
-  apiUrl: 'https://ecommerce.coding.global',
+  apiUrl: 'https://ecommerce.coding-global.com',
   keycloak: { ...keycloakConfig },
 };
